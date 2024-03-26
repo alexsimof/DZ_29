1f35705f243e6e6c076dcfa67b48bbae7c281e10
Alex Simonov <alex.simonof@gmail.com>
2024-03-26 15:37:13 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -46,23 +46,18 @@
 
 //
 
-const vehicle = new Vehicle();
-const car = new Car();
-const bike = new Bike();
-const sedan = new Sedan();
-const pickUp = new PickUp();
-const scooter = new Scooter();
-const chopper = new Chopper();
-
-function Vehicle() {
-  this.start = function() {
+const vehicle = {
+  start() {
     console.log('go-go!')
   },
-  this.stop = function() {
+  stop() {
     console.log('stop!')
-  }
+  },
 }
-function Car() {
+function Car(brand, type, year) {
+  this.brand = brand;
+  this.type = type;
+  this.year = year;
   this.turnOnLight = function() {
     console.log('Свет включен!')
   },
@@ -70,7 +65,10 @@ function Car() {
     console.log('Свет выключен!')
   }
 }
-function Bike() {
+function Bike(brand, type, year) {
+  this.brand = brand;
+  this.type = type;
+  this.year = year;
   this.openFuelTap = function() {
     console.log('вжиик')
   },
@@ -78,3 +76,73 @@ function Bike() {
     console.log('чик..)')
   }
 }
+
+
+Car.prototype = vehicle;
+const audi = new Car('audi', 'hatchback', 2020);
+const bike = new Bike('bmv', 'scooter', 2015);
+
+audi.start = Car.prototype.stop;
+console.log(audi)
+audi.start()
+
+
+// admin
+
+
+function User(username, email) {
+
+  this.username = username;
+  this.email = email;
+
+  this.login = function() {
+    if (system.users.includes(this)) {
+      
+      this.id = Math.round(Math.random()*10)
+      console.log('Вы вошли в систему!')
+    } else {
+      console.log('Вы не зарегистрированы в системе!')
+    }
+  },
+  this.logout = function() {
+    system.removeUser(this)
+    console.log('Вы вышли из системы!')
+  }
+}
+
+function System() {
+  this.users = [];
+
+  this.addUser = function(user) {
+    this.users.push(user)
+  }
+  this.removeUser = function(user) {
+    let usr = system.users.indexOf(user)
+    if (usr !== -1) { this.users.splice(usr, 1) }
+  }
+  this.getOnLineUsers = function() {
+    let id = this.users.map(user => user.id) 
+    console.log(id)
+  }
+  
+  this.getUser = function(id) {
+    console.log(id)
+  }
+
+}
+
+const system = new System()
+const alex = new User('alex', 'alex@uk.ua')
+const den = new User('den', 'den@gmail.com')
+
+system.addUser(alex)
+system.addUser(den)
+
+alex.login()
+den.login()
+
+system.removeUser(alex)
+
+console.log(system)
+
+system.getOnLineUsers()
